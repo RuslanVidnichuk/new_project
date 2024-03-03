f6067e3a09508ebe2c0d3059014cedac951a3d1e
Ruslan <monnaliza139@gmail.com>
2024-03-03 16:57:34 +0200

    zxczcx

--- js/gallery.js
+++ js/gallery.js
@@ -122,3 +122,15 @@ gallery.addEventListener("click", e => {
 
 instance.show()
 })
+const form = document.querySelector(".feedback-form");
+const localStorageKey = "goit-example-message";
+
+form.addEventListener("input", (evt) => {
+  localStorage.setItem(localStorageKey, evt.target.value);
+});
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+	console.log(evt.target.elements.message.value);
+  form.reset();
+});
